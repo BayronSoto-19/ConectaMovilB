9e789aa58d655c5285156ba84b7e910a0c79084b
BayronSoto-19 <b.soto54@alumnos.santotomas.cl>
2023-12-06 23:21:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/conectamovilb/Modelo/Usuario.java
@@ -0,0 +1,4 @@
+package com.example.conectamovilb.Modelo;
+
+public class Usuario {
+}
---
+++ app/src/main/java/com/example/conectamovilb/Vista/Perfil.java
@@ -0,0 +1,16 @@
+package com.example.conectamovilb.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.conectamovilb.R;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
